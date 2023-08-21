be10a11212be363e9550716ebbbe3603f97285b4
ADAllendez <alejo.diaz80.ad@gmail.com>
2023-08-21 19:29:51 -0300

    Tp1

---
+++ src/pages/layouts/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFoundImg from '../../assets/images/404.jpg';
+const NotFound = () => {
+    return (
+        <>
+            <img style={{height:'100vh',width:'100%'}} src={NotFoundImg} />
+        </>
+    )
+}
+export default NotFound
